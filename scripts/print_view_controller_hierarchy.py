a7f5315633b5b3787593205cc7dc540023baf7ad
mrhappyasthma <mrhappyasthma@gmail.com>
2017-04-30 01:04:22 -0700

    Update the script to grab the results. This is needed for the tests.

--- scripts/print_view_controller_hierarchy.py
+++ scripts/print_view_controller_hierarchy.py
@@ -3,8 +3,7 @@
 Usage: pvc
 """
 def print_view_controller_hierarchy(debugger, command, result, internal_dict):
-  debugger.HandleCommand('po [[[UIWindow keyWindow] rootViewController] _printHierarchy]')
-
+  debugger.GetCommandInterpreter().HandleCommand('po [[[UIWindow keyWindow] rootViewController] _printHierarchy]', result)
 
 def __lldb_init_module(debugger, internal_dict):
   debugger.HandleCommand('command script add -f print_view_controller_hierarchy.print_view_controller_hierarchy pvc')
