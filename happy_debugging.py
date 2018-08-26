b2525102362c45ccfa29e8f08caa0030b7d6c2ee
mrhappyasthma <mrhappyasthma@gmail.com>
2018-08-26 01:55:14 -0700

    Add a custom Println method to lldb.SBCommandReturnObject.

--- happy_debugging.py
+++ happy_debugging.py
@@ -1,7 +1,15 @@
 import glob
+import lldb
 import os
 
+def Println(self, *args):
+  self.Print(args[0] + '\n')
+
 def __lldb_init_module(debugger, internal_dict):
+  # Add a custom print function to SBCommandReturnObject.
+  lldb.SBCommandReturnObject.Println = Println
+
+  # Load all of the lldb scripts.
   scripts_wildcard = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                   'scripts/*.py')
   scripts = glob.glob(scripts_wildcard)
--- run_all_tests.py
+++ run_all_tests.py
@@ -4,8 +4,17 @@
 import os
 import unittest
 
+from tests.test_utils import import_utils
+import_utils.prepare_lldb_import_or_exit()
+
+import happy_debugging
+import lldb
 
 def main():
+  # Add the custom print function to SBCommandReturnObject.
+  lldb.SBCommandReturnObject.Println = happy_debugging.Println
+
+  # Execute all tests.
   test_loader = unittest.TestLoader()
   tests_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tests')
   test_suite = test_loader.discover(tests_dir, '*.py')
